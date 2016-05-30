a50844266ce158e72995af0b92ea1fad9f40ce40
mc12345678 <mc12345678@users.noreply.github.com>
2016-05-29 20:05:14 -0400

    Add/update languages with options added to English admin language files.

--- admin/includes/languages/french/extra_definitions/products_with_attributes.php
+++ admin/includes/languages/french/extra_definitions/products_with_attributes.php
@@ -4,6 +4,7 @@
  */
 define('BOX_CATALOG_PRODUCTS_WITH_ATTRIBUTES_STOCK', 'Stock de Produits avec Attributs (aka SBA)');
 define('BOX_CONFIGURATION_PRODUCTS_WITH_ATTRIBUTES_STOCK_SETUP', 'Stock de Produits avec Attributs (aka SBA) Setup');
+define('BOX_CONFIGURATION_PRODUCTS_WITH_ATTRIBUTES_STOCK_AJAX', 'Stock de Produits avec Attributs Ajax');
 define('IMAGE_OPTION_SBA', 'Link to SBA');
 define('TEXT_SBA_EDIT', 'SBA THIS PRODUCT');
 define('TEXT_NO_SBA_EDIT', 'Product Not SBA Tracked');
