56b5ab4144143600a223658fe25723bf70628059
mc12345678 <mc12345678@users.noreply.github.com>
2016-06-10 22:23:14 -0400

    Incorporate change made to ZC 1.5.5.a the zen_get_show_product_switch_name and zen_get_show_product_switch had parameters that did not make sense as the word prefix was used for a parameter that provided a suffix and vice versa.

--- 1_5_5/includes/functions/functions_lookups.php
+++ 1_5_5/includes/functions/functions_lookups.php
@@ -704,7 +704,7 @@
 /*
  * Look up SHOW_XXX_INFO switch for product ID and product type
  */
-    function zen_get_show_product_switch_name($lookup, $field, $suffix= 'SHOW_', $prefix= '_INFO', $field_prefix= '_', $field_suffix='') {
+    function zen_get_show_product_switch_name($lookup, $field, $prefix= 'SHOW_', $suffix= '_INFO', $field_prefix= '_', $field_suffix='') {
       global $db;
 
       $sql = "select products_type from " . TABLE_PRODUCTS . " where products_id='" . (int)$lookup . "'";
@@ -714,7 +714,7 @@
       $show_key = $db->Execute($sql);
 
 
-      $zv_key = strtoupper($suffix . $show_key->fields['type_handler'] . $prefix . $field_prefix . $field . $field_suffix);
+      $zv_key = strtoupper($prefix . $show_key->fields['type_handler'] . $suffix . $field_prefix . $field . $field_suffix);
 
       return $zv_key;
     }
@@ -722,7 +722,7 @@
 /*
  * Look up SHOW_XXX_INFO switch for product ID and product type
  */
-    function zen_get_show_product_switch($lookup, $field, $suffix= 'SHOW_', $prefix= '_INFO', $field_prefix= '_', $field_suffix='') {
+    function zen_get_show_product_switch($lookup, $field, $prefix= 'SHOW_', $suffix= '_INFO', $field_prefix= '_', $field_suffix='') {
       global $db;
 
       $sql = "select products_type from " . TABLE_PRODUCTS . " where products_id='" . $lookup . "'";
@@ -732,7 +732,7 @@
       $show_key = $db->Execute($sql);
 
 
-      $zv_key = strtoupper($suffix . $show_key->fields['type_handler'] . $prefix . $field_prefix . $field . $field_suffix);
+      $zv_key = strtoupper($prefix . $show_key->fields['type_handler'] . $suffix . $field_prefix . $field . $field_suffix);
 
       $sql = "select configuration_key, configuration_value from " . TABLE_PRODUCT_TYPE_LAYOUT . " where configuration_key='" . $zv_key . "'";
       $zv_key_value = $db->Execute($sql);
