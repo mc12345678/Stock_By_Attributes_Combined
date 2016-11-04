2c797fa8355b605a99fdecc83c7fe09d1b21f6f3
mc12345678 <mc12345678@users.noreply.github.com>
2016-11-04 11:14:38 -0400

    Admin javascript reworked and overhauled.
    - Code sequenced to "pre-load" before use.
    - Code referencing non-existent function(s) commented out to
      prevent browser load error(s).
    - Use of JS variable this, reduced to be more specific in
      sections of code known to be used in production.
    - Set focus to the field being clicked on when clicking to
      edit a table value.
    - Set focus on page load to the search box.
    - Validated that code display was offset by 4 spaces throughout.

--- admin/products_with_attributes_stock_ajax.js
+++ admin/products_with_attributes_stock_ajax.js
@@ -1,190 +1,169 @@
 // $Id: products_with_attributes_stock_ajax.js 389 2008-11-14 16:02:14Z hugo13 $
 //Stock by Attributes 1.5.4 (supports jquery-1.10.2.min.js)
 
-$(document).ready(function() { 
-    // bind form using ajaxForm 
-    $('#pwas-search').ajaxForm({ 
-        // target identifies the element(s) to update with the server response 
-        target: '#pwa-table',
-        url: 'products_with_attributes_stock_ajax.php', 
-        success:  addEvent
- 
-        // success identifies the function to invoke when the server response 
-        // has been received; here we apply a fade-in effect to the new content 
-    });
-});
-
-$(document).ready(function() { 
-    // bind form using ajaxForm 
-    $('#store').ajaxForm({ 
-        // target identifies the element(s) to update with the server response 
-        target: '#hugo1', 
-        success: saved
- 
-        // success identifies the function to invoke when the server response 
-        // has been received; here we apply a fade-in effect to the new content 
-    }); 
-});
-
-
-$(document).ready(function(){
-//TODO: Look at the URL: should this be a random generator entry	
-    $("#btnrandom").click(function(e){
-        e.preventDefault(); // Normales Submit unterdrücken
-
-        $.ajax({ // AJAX Request auslösen
-            type: "POST",
-            url: '/data/random/5a0bc3836e07a7be06a2fc3109b9d9daaffeafda/1',
-            dataType: 'json',
-            global: 'false',
-            success: processJason
-        });
-    });
-    
-    $("#loading").hide();    // Das Loding Element verstecken
-    
-    //Quantity
-    $(".stockAttributesCellQuantity").click(function(event){
-        var $tgt = $(event.target);
-        var $id = this.id;
-        var $inner = this.innerHTML;
-        if (!$tgt.is('input')) {
-            if(!this.hasEventHander){
-            var $newLi = '<input type="text" name="' + $id + '" id="' + $id + '" value="' + $inner + '" size="8"/>';
-            this.innerHTML = $newLi;
-                this.hasEventHander = true;
-            }
-        }        
-    })
-    
-    //Sort
-    $(".stockAttributesCellSort").click(function(event){
-        var $tgt = $(event.target);
-        var $id = this.id;
-        var $inner = this.innerHTML;
-        if (!$tgt.is('input')) {
-            if(!this.hasEventHander){
-            var $newLi = '<input type="text" name="' + $id + '" id="' + $id + '" value="' + $inner + '" size="8"/>';
-            this.innerHTML = $newLi;
-                this.hasEventHander = true;
-            }
-        }        
-    })
-    
-    //stockAttributesCellCustomid
-    $(".stockAttributesCellCustomid").click(function(event){
-        var $tgt = $(event.target);
-        var $id = this.id;
-        var $inner = this.innerHTML;
-        if (!$tgt.is('input')) {
-            if(!this.hasEventHander){
-            var $newLi = '<input type="text" name="' + $id + '" id="' + $id + '" value="' + $inner + '" size="8"/>';
-            this.innerHTML = $newLi;
-                this.hasEventHander = true;
-            }
-        }        
-    })
-
-    //stockAttributesCellTitle
-    $(".stockAttributesCellTitle").click(function(event){
-        var $tgt = $(event.target);
-        var $id = this.id;
-        var $inner = this.innerHTML;
-        if (!$tgt.is('input')) {
-            if(!this.hasEventHander){
-            var $newLi = '<input type="text" name="' + $id + '" id="' + $id + '" value="' + $inner + '" size="25"/>';
-            this.innerHTML = $newLi;
-                this.hasEventHander = true;
-            }
-        }        
-    })
-    
-});
-
 //stockAttributesCellQuantity
 function addEvent() {
- $('.stockAttributesCellQuantity').each(
-            function(){
-                if(!this.hasEventHander)
-                    $(this).click(function(event){    
-                    /*/ Our Eventhanderl /*/    
-                    var $tgt = $(event.target);
-                    var $id = this.id;
-                    var $inner = this.innerHTML;
-                    if (!$tgt.is('input')) {
-                        var $newLi = '<input type="text" name="' + $id + '" id="' + $id + '" value="' + $inner + '" size="8"/>';
-                        this.innerHTML = $newLi;
-                        //this.unbind("click");
-                    }        
-                });
-                this.hasEventHander = true;
-            }
-        );
-}
+    "use strict";
+
+    $(".stockAttributesCellQuantity").each(function (iter) {
+        var curcelltitle = this;
+        if (!curcelltitle.haseventhandler) {
+            $(curcelltitle).click(function (event) {
+            /* Our Eventhanderl */
+                var tgt = event.target;
+                var id = tgt.id;
+                var inner = tgt.innerHTML;
+                if (!$(tgt).is("input")) {
+                    var newLi = "<input type=\"text\" name=\"" + id + "\" id=\"" + id + "\" value=\"" + inner + "\" size=\"8\"/>";
+                    tgt.innerHTML = newLi;
+                    //this.unbind("click");
+                }
+            });
+            curcelltitle.haseventhandler = true;
+        }
+    });
 
 //stockAttributesCellSort
-function addEvent() {
-	$('.stockAttributesCellSort').each(
-			function(){
-				if(!this.hasEventHander)
-					$(this).click(function(event){    
-					/*/ Our Eventhanderl /*/    
-	                var $tgt = $(event.target);
-	                var $id = this.id;
-	                var $inner = this.innerHTML;
-	                if (!$tgt.is('input')) {
-	                	var $newLi = '<input type="text" name="' + $id + '" id="' + $id + '" value="' + $inner + '" size="8"/>';
-	                    this.innerHTML = $newLi;
-	                    //this.unbind("click");
-	                }        
-					});
-	           this.hasEventHander = true;
-			}
-	);
-}
+    $(".stockAttributesCellSort").each(function (iter) {
+        var curcelltitle = this;
+        if (!curcelltitle.haseventhandler) {
+            $(curcelltitle).click(function (event) {
+            /* Our Eventhanderl */
+                var tgt = event.target;
+                var id = tgt.id;
+                var inner = tgt.innerHTML;
+                if (!$(tgt).is("input")) {
+                    var newLi = "<input type=\"text\" name=\"" + id + "\" id=\"" + id + "\" value=\"" + inner + "\" size=\"8\"/>";
+                    tgt.innerHTML = newLi;
+                    //this.unbind("click");
+                }
+            });
+            curcelltitle.haseventhandler = true;
+        }
+    });
 
 //stockAttributesCellCustomid
-function addEvent() {
-	$('.stockAttributesCellCustomid').each(
-			function(){
-				if(!this.hasEventHander)
-					$(this).click(function(event){    
-					/*/ Our Eventhanderl /*/    
-	                var $tgt = $(event.target);
-	                var $id = this.id;
-	                var $inner = this.innerHTML;
-	                if (!$tgt.is('input')) {
-	                	var $newLi = '<input type="text" name="' + $id + '" id="' + $id + '" value="' + $inner + '" size="8"/>';
-	                    this.innerHTML = $newLi;
-	                    //this.unbind("click");
-	                }        
-					});
-	           this.hasEventHander = true;
-			}
-	);
-}
+    $(".stockAttributesCellCustomid").each(function (iter) {
+        var curcelltitle = this;
+        if (!curcelltitle.haseventhandler) {
+            $(curcelltitle).click(function (event) {
+                /* Our Eventhanderl */
+                var tgt = event.target;
+                var id = tgt.id;
+                var inner = tgt.innerHTML;
+                if (!$(tgt).is("input")) {
+                    var newLi = "<input type=\"text\" name=\"" + id + "\" id=\"" + id + "\" value=\"" + inner + "\" size=\"8\"/>";
+                    tgt.innerHTML = newLi;
+                    //this.unbind("click");
+                }
+            });
+            curcelltitle.haseventhandler = true;
+        }
+    });
+//}
 
 //stockAttributesCellTitle
-function addEvent() {
-	$('.stockAttributesCellTitle').each(
-			function(){
-				if(!this.hasEventHander)
-					$(this).click(function(event){    
-					/*/ Our Eventhanderl /*/    
-	                var $tgt = $(event.target);
-	                var $id = this.id;
-	                var $inner = this.innerHTML;
-	                if (!$tgt.is('input')) {
-	                	var $newLi = '<input type="text" name="' + $id + '" id="' + $id + '" value="' + $inner + '" size="25"/>';
-	                    this.innerHTML = $newLi;
-	                    //this.unbind("click");
-	                }        
-					});
-	           this.hasEventHander = true;
-			}
-	);
+//function addEvent() {
+    $(".stockAttributesCellTitle").each(function (iter) {
+        var curcelltitle = this;
+        if (!curcelltitle.haseventhandler) {
+            $(curcelltitle).click(function (event) {
+                /* Our Eventhanderl */
+                var tgt = event.target;
+                var id = tgt.id;
+                var inner = tgt.innerHTML;
+                if (!$(tgt).is("input")) {
+                    var newLi = "<input type=\"text\" name=\"" + id + "\" id=\"" + id + "\" value=\"" + inner + "\" size=\"25\"/>";
+                    tgt.innerHTML = newLi;
+                    //this.unbind("click");
+                }
+            });
+            curcelltitle.haseventhandler = true;
+        }
+    });
 }
 
-function saved(responseText, statusText)  { 
-    alert('status: ' + statusText + '\n\nresponseText: \n' + responseText ); 
+function saved(responseText, statusText) {
+    "use strict";
+    alert("status: " + statusText + "\n\nresponseText: \n" + responseText);
 }
+
+$(document).ready(function () {
+    "use strict";
+    // bind form using ajaxForm
+    $("#pwas-search").ajaxForm({
+        // target identifies the element(s) to update with the server response
+        target: "#pwa-table",
+        url: "products_with_attributes_stock_ajax.php",
+        success: addEvent
+
+        // success identifies the function to invoke when the server response
+        // has been received; here we apply a fade-in effect to the new content
+    });
+});
+
+$(document).ready(function () {
+    "use strict";
+    // bind form using ajaxForm
+    $("#store").ajaxForm({
+        // target identifies the element(s) to update with the server response
+        target: "#hugo1",
+        success: saved
+
+        // success identifies the function to invoke when the server response
+        // has been received; here we apply a fade-in effect to the new content
+    });
+});
+
+
+$(document).ready(function () {
+    "use strict";
+//TODO: Look at the URL: should this be a random generator entry
+//    $("#btnrandom").click(function (e) {
+//        e.preventDefault(); // Normales Submit unterdrücken
+
+//        $.ajax({ // AJAX Request auslösen
+//            type: "POST",
+//            url: "/data/random/5a0bc3836e07a7be06a2fc3109b9d9daaffeafda/1",
+//            dataType: "json",
+//            global: "false",
+//            success: processJason
+//        });
+//    });
+
+    $("#loading").hide();    // Das Loding Element verstecken
+
+    //class identified item to be edited as an input text string
+    $(".editthis").click(function (event) {
+        var tgt = event.target;
+        var id = tgt.id;
+        var inner = tgt.innerHTML;
+        if (!$(tgt).is("input")) {
+            if (!tgt.haseventhandler) {
+                var newLi = "<input type=\"text\" name=\"" + id + "\" id=\"" + id + "\" value=\"" + inner + "\" size=\"8\"/>";
+                tgt.innerHTML = newLi;
+                tgt.haseventhandler = true;
+            }
+        }
+        $("#" + tgt.id + " input").focus();
+    });
+
+    //stockAttributesCellTitle
+    $(".stockAttributesCellTitle").click(function (event) {
+        var tgt = event.target;
+        var id = tgt.id;
+        var inner = tgt.innerHTML;
+        if (!$(tgt).is("input")) {
+            if (!tgt.haseventhandler) {
+                var newLi = "<input type=\"text\" name=\"" + id + "\" id=\"" + id + "\" value=\"" + inner + "\" size=\"25\"/>";
+                tgt.innerHTML = newLi;
+                tgt.haseventhandler = true;
+            }
+        }
+        $("#" + tgt.id + " input").focus();
+    });
+
+    $("#pwas-filter").focus();
+});
+
+
