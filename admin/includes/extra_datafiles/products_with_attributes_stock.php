8c8ade1636544839a3dcbc5fd4b11f73e40b269f
mc12345678 <mc12345678@users.noreply.github.com>
2017-07-03 15:00:20 -0400

    Add table define for admin side
    Table for non-stock product is defined on the store front, but needs to be defined in relation to the admin to support installation/generation of the table.

--- admin/includes/extra_datafiles/products_with_attributes_stock.php
+++ admin/includes/extra_datafiles/products_with_attributes_stock.php
@@ -8,5 +8,6 @@ if (!defined('IS_ADMIN_FLAG')) {
 define('FILENAME_PRODUCTS_WITH_ATTRIBUTES_STOCK','products_with_attributes_stock');
 define('TABLE_PRODUCTS_WITH_ATTRIBUTES_STOCK', DB_PREFIX . 'products_with_attributes_stock');
 define('TABLE_ORDERS_PRODUCTS_ATTRIBUTES_STOCK', DB_PREFIX . 'orders_products_attributes_stock');
+define('TABLE_PRODUCTS_WITH_ATTRIBUTES_STOCK_ATTRIBUTES_NON_STOCK', DB_PREFIX . 'products_with_attributes_stock_attributes_non_stock');
 define('FILENAME_PRODUCTS_WITH_ATTRIBUTES_STOCK_SETUP','stock_by_attr_install');
 define('FILENAME_PRODUCTS_WITH_ATTRIBUTES_STOCK_AJAX', 'products_with_attributes_stock_ajax');
