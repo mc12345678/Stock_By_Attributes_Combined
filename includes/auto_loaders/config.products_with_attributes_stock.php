59a274423cc1e3fb872c5bfc5244b97f9d5b099a
mc12345678 <mc12345678@users.noreply.github.com>
2019-09-26 07:24:40 -0400

    Adjust load points and add comment
    Identified in ZC 1.5.6 that the `zen_products_stock` notifier was not observed when the shopping_cart was being modified. This moves the observer to a point before the load of `init_cart_actions` which is what processes the shopping_cart actions.  Also aligned the execution of the class' reset to the same load point as the class while maintaining it after the point (in array population) where the class is loaded and kept the observer after the class to ensure loading the observer does not throw notices or errors when trying to access variables that would not have otherwise been defined on first page load.
    
    Fixes #59

--- includes/auto_loaders/config.products_with_attributes_stock.php
+++ includes/auto_loaders/config.products_with_attributes_stock.php
@@ -11,9 +11,9 @@
  * @copyright   Portions Copyright 2003 osCommerce
  * @link        http://www.zen-cart.com/
  * @license     http://www.zen-cart.com/license/2_0.txt GNU Public License V2.0
- * @version     $Id: config.products_with_attributes_stock.php xxxx 2013-06-28 20:31:10Z mc12345678 $
+ * @version     $Id: config.products_with_attributes_stock.php xxxx Modified 2019-09-26 mc12345678 $
  *
- * Stock by Attributes 1.5.4
+ * Stock by Attributes 1.5.5
  */
 
  $autoLoadConfig[0][] = array(
@@ -26,14 +26,16 @@
   'objectName' => 'pwas_class'
   );*/
 // Does it need to load as early as 78? Works there, but shouldn't it be 199?
- $autoLoadConfig[199][] = array(
+// Load the class at the same point (or before) as the observer which uses it. 
+ $autoLoadConfig[135][] = array(
   'autoType' => 'classInstantiate',
   'className' => 'products_with_attributes_class_stock',
   'objectName' => 'pwas_class2',
   'checkInstantiated' => true,
   'classSession'=>true
-  ); 
- $autoLoadConfig[200][] = array(
+  );
+// Perform cleanup before real use.
+ $autoLoadConfig[135][] = array(
   'autoType' => 'objectMethod',
   'objectName' => 'pwas_class2',
   'methodName' => '__construct'
@@ -42,8 +44,9 @@
   'autoType' => 'class',
   'loadFile' => 'observers/class.products_with_attributes_stock.php'
   );
- $autoLoadConfig[199][] = array(
+// Be sure that observer is available before first time notifier needs to be observed.
+ $autoLoadConfig[135][] = array(
   'autoType' => 'classInstantiate',
   'className' => 'products_with_attributes_stock',
   'objectName' => 'products_with_attributes_stock_observe'
-  ); 
+  );
