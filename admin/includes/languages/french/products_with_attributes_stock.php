2442c103de2c2b57afe784aafd4bcc3241041fee
mc12345678 <mc12345678@users.noreply.github.com>
2021-02-02 22:30:34 -0500

    Update/populate non-english language files

--- admin/includes/languages/french/products_with_attributes_stock.php
+++ admin/includes/languages/french/products_with_attributes_stock.php
@@ -9,6 +9,8 @@
  * Stock by Attributes 1.5.4 mc12345678 15-08-16
  */
  
+define('HEADING_TITLE', 'Stock By Attributes');
+
 define('PWA_DELETE_VARIANT_CONFIRMATION', 'Etes-vous s&ucirc;r que vous voulez supprimer cette variante de produit?');
 define('PWA_DELETE_VARIANTS_CONFIRMATION', 'Are you sure you want to delete each variant of this product?');
 
@@ -20,6 +22,7 @@ define('PWA_QUANTITY', 'Quantit&eacute;: ');
 define('PWA_QUANTITY_FOR_ALL_VARIANTS', 'Quantit&eacute; pour toutes les Variantes');
 define('PWA_ADD_QUANTITY', 'Ajouter Quantit&eacute; pour cette Variante du Produit');
 define('PWA_SYNC_QUANTITY', 'Synchronisation de la Quantiti&eacute;');
+define('PWA_TEXT_SEARCH', 'Search: ');
 define('TEXT_IMAGE_NONEXISTENT', 'Aucune image disponible');
 
 define('PWA_EDIT_QUANTITY', 'Editer la Quantit&eacute;');
@@ -41,3 +44,21 @@ define('PWA_PAC', 'Unique Combo');
 define('PWA_EDIT', 'Editer');
 define('PWA_DELETE', 'Supprimer');
 define('PWA_SUBMIT','Soumettre');
+
+define('PWA_QUANTITY_MISSING', 'Missing Quantity!');
+define('PWA_PRODUCTS_ID_BAD', 'Missing or bad products_id!');
+define('PWA_QUANTITY_BAD', 'Missing or bad Quantity!');
+define('PWA_ATTRIBUTE_MISSING', 'Missing Attribute Selection!');
+define('PWA_MIX_ERROR_ALL_COMBO', 'Do NOT mix \'All - Attributes\' and \'All - Attributes - Combo\'');
+define('PWA_NO_CHANGES', 'No changes made.');
+define('PWA_UPDATE_SUCCESS', 'Product successfully updated');
+define('PWA_UPDATE_FAILURE', 'Product %1$d update failed: %2$s');
+define('PWA_PARENT_QUANTITY_UPDATE_SUCCESS', 'Parent Product Quantity Updated');
+define('PWA_PARENT_QUANTITIES_UPDATE_SUCCESS', 'Parent Product Quantities Updated');
+define('PWA_SORT_UPDATE_SUCCESS', '%1$d stock attributes updated for sort by primary attribute sort order');
+define('PWA_ADJUST_QUANTITY_SUCCESS', 'Product %1$d with stock_id %2$d found on search \'%3$s\' has been updated by %4$f to a value of %5$f.');
+define('PWA_ADJUST_QUANTITY_NONE_FOUND', 'No product were found using search content of \'%1$s\' to adjust quantity by %2$f.');
+define('PWA_ADJUST_QUANTITY_MULTIPLE_NOT_SUPPORTED_YET', 'Multiple product were found using the search criteria \'%1$s\' to try to adjust by %2$f; however, this is not yet supported. Try a search that provides a single variant.');
+define('PWA_BUTTON_SEARCH', 'Search');
+define('PWA_BUTTON_ADJUST', 'Adjust');
+
