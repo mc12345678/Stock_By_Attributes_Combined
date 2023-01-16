91b8a7db2c8adb8ebf93f458012b23cad16b6281
mc12345678 <mc12345678@users.noreply.github.com>
2023-01-16 15:06:06 -0500

    Incorporate potential use of ZC 1.5.8 function(s)
    Added a test that if function does not exist then use the old method of
    testing if the constant is defined and then defining it if it isn't.
    
    If the function does exist then use it to attempt to define the variable
    if it is not yet defined.
    
    Reasoning for capturing the test this way is that in the future, the code
    could be truncated/modified to exclude the extra "older" code.

--- includes/extra_datafiles/dynamicdropdownsforsba.php
+++ includes/extra_datafiles/dynamicdropdownsforsba.php
@@ -1,2 +1,11 @@
 <?php
-if (!defined('TABLE_PRODUCTS_STOCK')) define('TABLE_PRODUCTS_STOCK', DB_PREFIX. 'products_with_attributes_stock');
+
+if (!function_exists('zen_define_default')) {
+    if (!defined('TABLE_PRODUCTS_STOCK')) {
+      define('TABLE_PRODUCTS_STOCK', DB_PREFIX . 'products_with_attributes_stock');
+    }
+    return;
+}
+
+zen_define_default('TABLE_PRODUCTS_STOCK', DB_PREFIX . 'products_with_attributes_stock');
+
