c77c524a79adca24f60a85365a889d7a3bacd044
mc12345678 <mc12345678@users.noreply.github.com>
2023-02-18 15:03:55 -0500

    Correct language conversion (#107)
    * Correct language conversion
    
    Process to convert former language files to those used by Zen Cart 1.5.8, didn't translate a die statement correctly and caused a php error when relied on as the only language file.
    
    * English
    
    * Dutch
    
    * French

--- admin/includes/languages/french/extra_definitions/lang.reg_ddsba.php
+++ admin/includes/languages/french/extra_definitions/lang.reg_ddsba.php
@@ -1,6 +1,6 @@
 <?php
 if (! defined('IS_ADMIN_FLAG')) {
-  die('Illegal Access',
+  die('Illegal Access');
 }
 if (!defined('BOX_CONFIGURATION_DYNAMIC_DROPDOWNS')) $define = [
     'BOX_CONFIGURATION_DYNAMIC_DROPDOWNS' => 'Dynamic Drop Downs',
